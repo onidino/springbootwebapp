88aafa6d22271dace3488b91748053d9512af3cb
onidino <lucianodefilippe@gmail.com>
2022-12-08 22:43:04 -0300

    html controller + utils for resource file

--- src/main/java/com/in28minutes/springboot/springbootwebapp/controller/SayHelloController.java
+++ src/main/java/com/in28minutes/springboot/springbootwebapp/controller/SayHelloController.java
@@ -1,14 +1,21 @@
 package com.in28minutes.springboot.springbootwebapp.controller;
 
+import com.in28minutes.springboot.springbootwebapp.utils.Utils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SayHelloController {
 
+
   @GetMapping("say-hello")
   public String sayHello() {
     return "Hello! What are you learning today?";
+  }
+
+  @GetMapping("say-hello-html")
+  public String sayHelloHtml() {
+    return Utils.getResourceFileAsString("sayhello.html");
   }
 
 }
---
+++ src/main/java/com/in28minutes/springboot/springbootwebapp/utils/Utils.java
@@ -0,0 +1,23 @@
+package com.in28minutes.springboot.springbootwebapp.utils;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Utils {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(Utils.class);
+
+  public static String getResourceFileAsString(final String filename) {
+    try {
+      return Files.readString(Path.of(
+              String.format("src/main/resources/html/%s", filename)),
+          StandardCharsets.UTF_8);
+    } catch (Exception e) {
+      LOGGER.info("Error getting the file {}", filename);
+    }
+    return null;
+  }
+}
