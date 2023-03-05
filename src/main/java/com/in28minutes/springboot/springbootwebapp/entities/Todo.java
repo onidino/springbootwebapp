e44e278aa4295322f14444df98dca86dbd478bfe
onidino <lucianodefilippe@gmail.com>
2023-03-05 14:21:12 -0300

    adding h2 test data in TODO table

--- src/main/java/com/in28minutes/springboot/springbootwebapp/entities/Todo.java
+++ src/main/java/com/in28minutes/springboot/springbootwebapp/entities/Todo.java
@@ -1,18 +1,29 @@
 package com.in28minutes.springboot.springbootwebapp.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@Entity(name = "Todo")
 public class Todo {
 
+  @Id
+  @GeneratedValue
   private int id;
   private String username;
   @Size(min = 10, message = "Enter at least 10 characters")
   private String description;
   private LocalDate targetDate;
   private boolean done;
+
+  protected Todo() {
+  }
 
   public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
     this.id = id;
@@ -33,3 +44,4 @@ public class Todo {
         '}';
   }
 }
+
---
+++ src/main/resources/testdata.sql
@@ -0,0 +1,14 @@
+insert into TODO (
+  ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (
+  1001, 'Test', 'Test TODO 1', CURRENT_DATE(), false);
+
+insert into TODO (
+  ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (
+  1002, 'Test', 'Test TODO 2', CURRENT_DATE(), false);
+
+insert into TODO (
+  ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (
+  1003, 'Test', 'Test TODO 3', CURRENT_DATE(), false);
